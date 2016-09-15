8b06168bf4049128f00528873c53d1674b59a331
Ardhitya Wiedha Irawan <ardhityawiedhairawan@gmail.com>
2016-09-15 14:19:42 +0700

    Initial Commit

--- app/src/main/java/webid/aiueo/timeago/DividerItemDecoration.java
+++ app/src/main/java/webid/aiueo/timeago/DividerItemDecoration.java
@@ -8,10 +8,6 @@ import android.graphics.drawable.Drawable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-
-/**
- * Created by Lincoln on 30/10/15.
- */
 public class DividerItemDecoration extends RecyclerView.ItemDecoration {
 
     private static final int[] ATTRS = new int[]{
